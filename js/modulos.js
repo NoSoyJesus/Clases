1c3a3263b62e6ad3d1517cf1ffb2c859a2170893
Jesus <jesusdanielba29@gmail.com>
2025-04-27 21:18:20 -0300

    html cambiado, css añadido

--- js/modulos.js
+++ js/modulos.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const header = '<img src="/img/solidarity_5208516.png" alt="Logo"><nav><ul><li><a href="/index.html">Inico</a></li><li><a href="/html/a.html">Prueba</a></li><li><a href="/html/b.html">Prueba2</a></li></ul></nav>';
-    const footer = '<footer><p>Jesus Baute y Patricio Muras</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p></footer>';
+    const header = '<img src="/img/pokemon-2.png" alt="Logo"><nav><ul><li><a href="/index.html">Equipo</a></li><li><a href="/html/a.html">Bolsa</a></li><li><a href="/html/b.html">Perfil</a></li></ul></nav>';
+    const footer = '<footer><p>Jesus Baute y Patricio Muras</p><p>© 2025 Liga Pokémon</p></footer>';
     document.querySelector('header').innerHTML = header;
     document.querySelector('footer').innerHTML = footer;
 });
